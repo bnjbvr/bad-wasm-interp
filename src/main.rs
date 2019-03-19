99e31ac9cf62365324910c8c9c67c6a37afc015d
Benjamin Bouvier <public@benj.me>
2019-03-19 17:20:55 +0100

    moar

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ struct Module {
     binary: Vec<u8>,
 }
 
+enum EvalResult {
+    I32(i32),
+}
+
 const TYPE_SECTION_ID: u32 = 1;
 
 impl Module {
@@ -14,8 +18,8 @@ impl Module {
             self.binary[*cur + 1] != 97 ||  // A
             self.binary[*cur + 2] != 115 || // S
             self.binary[*cur + 3] != 109
+        // M
         {
-            // M
             println!("error when checking header: expected 0xasm");
             return false;
         }
@@ -90,12 +94,7 @@ impl Module {
         );
     }
 
-    fn call(&self, func_index: usize) -> Result<(), &'static str> {
-        let mut cur = 0;
-        if !self.check_header(&mut cur) {
-            return Err("checking header failed");
-        }
-
+    fn decode_type_section(&self, mut cur: &mut usize) -> Result<u32, &'static str> {
         if !self.read_section_header(&mut cur, TYPE_SECTION_ID) {
             return Err("type section not found");
         }
@@ -113,23 +112,29 @@ impl Module {
             i += 1;
         }
 
-        // Function declaration section.
+        Ok(num_types)
+    }
+
+    fn decode_func_decl(&self, num_func_types: u32, mut cur: &mut usize) -> usize {
         self.read_section_header(&mut cur, 3);
-        let num_defs = self.read_var_u32(&mut cur);
-        for i in 0..num_defs {
+        let num_decls = self.read_var_u32(&mut cur);
+        for i in 0..num_decls {
             let func_type_index = self.read_var_u32(&mut cur);
-            if func_type_index > num_types {
-                return Err("unexpected function signature index");
+            if func_type_index > num_func_types {
+                println!("unexpected function signature index");
+                return usize::max_value();
             }
         }
+        num_decls as usize
+    }
 
-        // Export section.
+    fn decode_exports_section(&self, num_decls: u32, mut cur: &mut usize) {
         self.read_section_header(&mut cur, 7);
         let num_exports = self.read_var_u32(&mut cur);
         for i in 0..num_exports {
             // skip name
             let num_bytes = self.read_var_u32(&mut cur);
-            cur += num_bytes as usize;
+            *cur += num_bytes as usize;
 
             // export kind
             let export_kind = self.read_var_u32(&mut cur);
@@ -137,16 +142,100 @@ impl Module {
                 println!("not a function");
             }
 
-            // TODO func_index is a var u32, check it's correct,
             let func_index = self.read_var_u32(&mut cur);
+            if func_index > num_decls {
+                println!("one exported function doesn't exist!");
+            }
+        }
+    }
+
+    fn decode_func_body(&self, must_interpret: bool, mut cur: &mut usize) -> Option<EvalResult> {
+        let body_size = self.read_var_u32(cur);
+        if !must_interpret {
+            // skip the bytes.
+            *cur += body_size as usize;
+            return None;
         }
 
+        let num_local_groups = self.read_var_u32(cur);
+        for i in 0..num_local_groups {
+            let group_count = self.read_var_u32(cur);
+            // skip code (one u8).
+            *cur += 1;
+        }
+
+        let mut virtual_stack = Vec::new();
+
+        loop {
+            if *cur + 1 > self.binary.len() {
+                println!("error: exhausted bytes before reaching an End");
+            }
+
+            let opcode = self.binary[*cur];
+            *cur += 1;
+
+            match opcode {
+                0x01 => {
+                    // Nope nope nope.
+                }
+                0x0b => {
+                    // End opcode: terminate current block/loop.
+                    let result = virtual_stack.pop().expect("missing result");
+                    // TODO check that there aren't unused operands on the virtual stack.
+                    return Some(EvalResult::I32(result));
+                }
+                0x41 => {
+                    // i32.const
+                    let num = self.read_var_u32(cur) as i32;
+                    virtual_stack.push(num);
+                }
+                0x6a => {
+                    // i32.add
+                    let a = virtual_stack.pop().expect("missing operand");
+                    let b = virtual_stack.pop().unwrap();
+                    let num = a + b;
+                    virtual_stack.push(num);
+                }
+                _ => unreachable!(format!("unknown opcode: {}", opcode)),
+            }
+        }
+    }
+
+    fn decode_code_section(
+        &self,
+        callee_index: u32,
+        mut cur: &mut usize,
+    ) -> Result<(), &'static str> {
         if !self.read_section_header(&mut cur, 10) {
             println!("was expecting code section");
             return Err("expected code section");
         }
 
-        // TODO implement code section
+        let num_func_defs = self.read_var_u32(&mut cur);
+        // TODO does the number of function definitions match the previous number of function
+        // declarations?
+        for i in 0..num_func_defs {
+            let must_interpret = i == callee_index;
+            match self.decode_func_body(must_interpret, &mut cur) {
+                Some(result) => match result {
+                    EvalResult::I32(n) => println!("i32 result is {}", n),
+                },
+                None => {}
+            }
+        }
+        Ok(())
+    }
+
+    fn call_func(&self, func_index: usize) -> Result<(), &'static str> {
+        let mut cur = 0;
+        if !self.check_header(&mut cur) {
+            return Err("checking header failed");
+        }
+
+        let num_types = self.decode_type_section(&mut cur).unwrap();
+        let num_decls = self.decode_func_decl(num_types, &mut cur);
+        self.decode_exports_section(num_decls as u32, &mut cur);
+        self.decode_code_section(func_index as u32, &mut cur);
 
         Ok(())
     }
@@ -160,5 +249,5 @@ fn main() {
     ];
 
     let m = Module::new(binary);
-    m.call(0);
+    m.call_func(0);
 }
